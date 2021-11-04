efc0e60c63753fae6973c7d01823c81c991f97ae
Evan <wxwcoding@gamil.com>
2021-11-04 23:37:20 +0800

    feat: js to ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 
-export const isFalsy = (value) => (value === 0 ? false : !value)
-export const cleanObject = (object) => {
+export const isFalsy = (value: any) => (value === 0 ? false : !value)
+export const cleanObject = (object: object) => {
   const result = { ...object }
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key]
+    // @ts-ignore
     if (isFalsy(value)) delete result[key]
   })
   return result
 }
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [newValue, setNewValue] = useState(value)
   useEffect(() => {
     const timer = setTimeout(() => setNewValue(value), delay)
